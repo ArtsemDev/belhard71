b911708ff180fd9b2610b1e20ddafad4fcd65e06
ArtsemDev <thommy.python.dev@gmail.com>
2023-09-26 18:38:59 +0300

    resolve 5_3 task

--- lesson5.py
+++ lesson5.py
@@ -193,3 +193,13 @@
 # 12 14 16 18 20
 # 22 24 26 28 30
 # 32 34
+
+#
+# n = int(input("enter n: "))
+# for i in range(2, n + 1, 10):
+#     for j in range(i, i + 9, 2):
+#         if j <= n:
+#             print(j, end=" ")
+#         else:
+#             break
+#     print()
